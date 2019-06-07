a82fecc3e88e6177836a79b57d37711d31600cc4
Vincent Cadoret <vinnividivicci@users.noreply.github.com>
2019-06-07 17:52:00 -0400

    Working version that uses OAuth 2.0 and gets all spreadsheets in a user's drive using an optional filter.

---
+++ BIMOne.GoogleSheets/Drive.cs
@@ -0,0 +1,119 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+
+namespace BIMOne.GoogleSheets
+{
+    public static class Drive
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.DriveReadonly, SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "BIM One Google Sheets";
+        //static List<string> spreadsheetsList = new List<string>();
+
+        [MultiReturn(new[] { "fileNames", "fileIds" })]
+        public static Dictionary<string, object> GetGoogleSpreadsheets(string filter)
+        {
+            var fileNames = new List<string>();
+            var fileIds= new List<string>();
+            //spreadsheetsList.Clear();
+            UserCredential credential;
+            string credentialsPath = String.Format("{0}{1}{2}", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"\Dynamo\Dynamo Revit\2.0\packages\BIMOne.GoogleSheets\bin\", "credentials.json");
+
+            using (var stream =
+                new FileStream(credentialsPath, FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Get all spreadsheets from drive
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 1000;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+            listRequest.Q = String.Format("mimeType='application/vnd.google-apps.spreadsheet' and name contains '{0}'", filter);
+            listRequest.OrderBy = "name";
+
+
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    //spreadsheetsList.Add(String.Format("{0} ({1})", file.Name, file.Id));
+                    fileNames.Add(file.Name);
+                    fileIds.Add(file.Id);
+                }
+            }
+            else
+            {
+                fileNames.Add("No sheets found");
+                fileIds.Add("No sheets found");
+                //spreadsheetsList.Add("No sheets found");
+            }
+            //return spreadsheetsList;
+            var d = new Dictionary<string, object>();
+            d.Add("fileNames", fileNames);
+            d.Add("fileIds", fileIds);
+            return d;
+
+            // Define request parameters.
+            //String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            //String range = "Class Data!A2:E";
+            //SpreadsheetsResource.ValuesResource.GetRequest request =
+            //        service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            //ValueRange response = request.Execute();
+            //IList<IList<Object>> values = response.Values;
+            //if (values != null && values.Count > 0)
+            //{
+            //    rows.Add("Name, Major");
+            //    foreach (var row in values)
+            //    {
+            //        // Print columns A and E, which correspond to indices 0 and 4.
+            //        rows.Add(String.Format("{0}, {1}", row[0], row[4]));
+            //    }
+            //    return rows;
+            //}
+            //else
+            //{
+            //    return rows; 
+            //}
+        }
+    }
+}
