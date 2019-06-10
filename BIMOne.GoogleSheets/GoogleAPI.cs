507cd46596f85702192b2ac2f25bede236c7780c
Vincent Cadoret <vinnividivicci@users.noreply.github.com>
2019-06-10 15:55:33 -0400

    Added some documenting comments.

--- BIMOne.GoogleSheets/GoogleAPI.cs
+++ BIMOne.GoogleSheets/GoogleAPI.cs
@@ -23,7 +23,16 @@ namespace BIMOne
         static string[] Scopes = { SheetsService.Scope.DriveReadonly, SheetsService.Scope.Spreadsheets };
         static string ApplicationName = "BIM One Google Sheets";
         static string credentialsPath = String.Format("{0}{1}{2}", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"\Dynamo\Dynamo Revit\2.0\packages\BIMOne.GoogleAPI\bin\", "credentials.json");
-
+        
+        /// <summary>
+        /// Gets a list of Google Sheets present in a user's Google Drive with optional
+        /// 'contains' keyword filter.
+        /// </summary>
+        /// <param name="filter">The text filter to use when seraching for Google Sheets on the Drive.</param>
+        /// <returns>fileNames, fileIds as lists.</returns>
+        /// <search>
+        /// google, sheets, drive, read
+        /// </search>
         [MultiReturn(new[] { "fileNames", "fileIds" })]
         public static Dictionary<string, object> GetGoogleSpreadsheets(string filter = "")
         {
@@ -82,6 +91,17 @@ namespace BIMOne
             return d;
         }
 
+        /// <summary>
+        /// Writes a nested list of lists to a Google Spreadsheet
+        /// </summary>
+        /// <param name="spreadsheetId">The ID of the Spreadsheet (long unique identifier as string)</param>
+        /// <param name="sheet">The name of the sheet within the spreadsheet as string. Ex.: Sheet1 </param>
+        /// <param name="range">The range where to write the data as string. Ex.: A:Z</param>
+        /// <param name="data">A list of lists containing the data to write to Google Sheets.</param>
+        /// <returns>response</returns>
+        /// <search>
+        /// google, sheets, drive, write
+        /// </search>
         [MultiReturn(new[] { "response" })]
         public static Dictionary<string, object> WriteGoogleSheets(string spreadsheetId, string sheet, string range, List<IList<object>>data)
         {
